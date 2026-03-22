a4a6fed55aad1039119ed1a2f851160829800452
= <=>
2026-03-22 07:12:50 +0300

    I add database seeder for users

---
+++ database/seeders/Userseeder.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class Userseeder extends Seeder
+{
+ 
+    public function run(): void
+    {
+        \App\Models\User::factory(100)->create();
+    }
+}
